4218ce4fa292ab94e1fb8122d1cdd8d7ec1bc8f4
Gabriel <gabrielluisfsouza@gmail.com>
2019-11-03 22:29:47 -0300

    finishing project

--- main.py
+++ main.py
@@ -1,49 +1,60 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
 from http.client import OK, UNPROCESSABLE_ENTITY
-from Crypto.Cipher import PKCS1_OAEP
 import uuid
 from os import getenv
 
 
 app = Flask(__name__)
 
-with open("./public_rsa") as file:
-    chave_publica = RSA.importKey("".join(line for line in file))
+with open("./public_rsa.pem") as file:
+    chave_publica = RSA.importKey(file.read())
+    cifra_publica = PKCS1_OAEP.new(chave_publica)
 
-with open("./private_rsa") as file:
-    chave_privada = RSA.importKey("".join(line for line in file))
+with open("./private_rsa.pem") as file:
+    chave_privada = RSA.importKey(file.read())
+    cifra_privada = PKCS1_OAEP.new(chave_privada)
 
 with open("./aes_key", "wb+") as file:
-    key = chave_publica.encrypt(getenv("AES_KEY"))
+    key = cifra_publica.encrypt(bytes(getenv("AES_KEY"), encoding='utf-8'))
     file.write(key)
 
 
 @app.route('/encrypt', methods=['POST'])
 def encrypt():
-    body = request.data
-    aes_cypher = AES.new(chave_privada.decrypt(key))
+    content = request.data
+
+    length = len(content)
+    length %= 16
+    if length > 0:
+        content += b'\x80' + b'\0' * (15 - length)
+    aes_cypher = AES.new(cifra_privada.decrypt(key), AES.MODE_CBC, IV=b'\0' * 16)
 
     filename = uuid.uuid4()
-    with open(f"files/{filename}.txt", "wb+") as file:
-        file.write(aes_cypher.encrypt(body))
+    with open(f"files/{filename}.bin", "wb+") as file:
 
-    return f"{filename}", OK
+        encrypted_content = aes_cypher.encrypt(content)
+        file.write(encrypted_content)
+
+    return jsonify({"encryptedName": f"{filename}", "encryptedContent": str(encrypted_content)}), OK
 
 
-@app.route('/decrypt/{filename}', methods=['POST'])
+@app.route('/decrypt/<filename>', methods=['GET'])
 def decrypt(filename):
-    
+
     try:
         filename = uuid.UUID(filename)
     except ValueError():
         abort("Nome de arquivo invalido", UNPROCESSABLE_ENTITY)
 
-    with open(f"files/{filename}.txt", "") as file:
-        return file.read(), OK
+    with open(f"files/{filename}.bin", "rb") as f:
+        content = f.read()
 
-    
+    aes_cypher = AES.new(cifra_privada.decrypt(key), AES.MODE_CBC, IV=b'\0' * 16)
+
+    return aes_cypher.decrypt(content), OK
+
 
 if __name__ == "__main__":
     app.run()
